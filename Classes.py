24d682711024c3b4439939c8a187393daebe59b0
Muneeb Alam <muneebalam@live.com>
2016-09-19 18:05:08 +0200

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,67 +1,95 @@
-class VPP:
-    class HouseHolds:
-        def __init__(self,name):
-            self.name=name
-        class PD:
-            def __init__(self,name,power,price):
-                self.name=name
-                self.power = power
-                self.price=price
-        class DSM:
-            def __init__(self,name,power,price):
-                self.name=name
-                self.power = power
-                self.price=price
-
+class VirtualPowerPlant:
+    
+    def __init__(self,Name,ProfitsNormal,ProfitsPrimaryReserve,ProfitsGridStorage,Profits):
+        self.Name=Name
+        self.ProfitsNormal=ProfitsNormal
+        self.ProfitsPrimaryReserve=ProfitsPrimaryReserve
+        self.ProfitsGridStorage=ProfitsGridStorage
+        self.Profits=ProfitsNormal+ProfitsPrimaryReserve+ProfitsGridStorage
         
-        class BatteryStorage:
-            def __init__(self,name,power,price,rated_capacity_in_kWh,ageing_factor,usable_capacity_in_kWh,percentage_current_capacity,capacity_for_grid,charging_price,discharging_price,profit):
-                self.name=name
-                self.power = power
-                self.price=price
-                self.rated_capacity_in_kWh=rated_capacity_in_kWh
-                self.ageing_factor=ageing_factor
-                self.usable_capacity_in_kWh=rated_capacity_in_kWh*ageing_factor
-                self.percentage_current_capacity=percentage_current_capacity
-                self.capacity_for_grid=capacity_for_grid
-                self.charging_price=charging_price
-                self.discharging_price=discharging_price
-                self.profit=profit
-               
-        class SolarGeneratingUnit:
-            def __init__(self,name,power,price,profit):
-                self.name=name
-                self.power = power
-                self.price=price
-                self.profit=profit
-
-        class KWK:
-            def __init__(self,name,power,price,profit):
-                self.name=name
-                self.power = power
-                self.price=price
-                self.profit=profit
-
-    class Grid:
+        class Household:  
+            def __init__(self,Name,ProfitsFromPR,ProfitsFromGridSupport,BuyingPrice,Profit):
+                self.Name=Name
+                self.ProfitsFromPR=ProfitsFromPR
+                self.ProfitsFromGridSupport=ProfitsFromGridSupport
+                self.BuyingPrice=BuyingPrice
+                self.Profit=ProfitsFromPR+ProfitsFromGridSupport-BuyingPrice
                 
-        class Common_Grid:
-            def __init__(self,name,power,price,feed_in_price,draw_out_price,profit):
-                self.name=name
-                self.power = power
-                self.price=price        
-                self.feed_in_price=feed_in_price
-                self.draw_out_price=draw_out_price
-                self.profit=profit
-               
+            class StandardConsumingDevices:
+                def __init__(self,Name,Power,Price):
+                    self.Name=Name
+                    self.Power = Power
+                    self.Price=Price
+                
+            class DSM:
+                def __init__(self,Name,Power,Price,FreezerTemperature):
+                    self.Name=Name
+                    self.Power = Power
+                    self.Price=Price
+                    self.FreezerTemperature=FreezerTemperature
+    
+            class MarketBatteryStorage:
+                def __init__(self,Name,Power,Price,rated_capacity_in_kWh,AgeingFactor,UsableCapacityInKWh,PercentageCurrentCapacity,CapacityForGrid,ChargingPrice,DischargingPrice,Profit,ParticipationinPR):
+                    self.Name=Name
+                    self.Power = Power
+                    self.Price=Price
+                    self.rated_capacity_in_kWh=rated_capacity_in_kWh
+                    self.AgeingFactor=AgeingFactor
+                    self.UsableCapacityInKWh=rated_capacity_in_kWh*AgeingFactor
+                    self.PercentageCurrentCapacity=PercentageCurrentCapacity
+                    self.CapacityForGrid=CapacityForGrid
+                    self.ChargingPrice=ChargingPrice
+                    self.DischargingPrice=DischargingPrice
+                    self.Profit=Profit
+                    self.ParticipationinPR=ParticipationinPR
+                    
+            class MarketSolarGeneratingUnit:
+                def __init__(self,Name,Power,Price,Profit):
+                    self.Name=Name
+                    self.Power = Power
+                    self.Price=Price
+                    self.Profit=Profit
+            
+            class MarketCogenerationUnit:
+                def __init__(self,Name,Power,Price,Profit):
+                    self.Name=Name
+                    self.Power=Power
+                    self.Price=Price
+                    self.Profit=Profit
+
+            class FeedInTariff:
+                def __init__(self,Name,Power,Price,FeedInPremier,Profit):
+                    self.Name=Name
+                    self.Power=Power
+                    self.Price=Price        
+                    self.FeedInPremier=FeedInPremier
+                    self.Profit=Profit
+                    
+            class Grid:
+                def __init__(self,Name,Power,Price):
+                    self.Name=Name
+                    self.Power=Power
+                    self.Price=Price
+
+class ElectricityStockExchange:
+    def __init__(self,Name,ExchangePriceScenario,PrimaryReserveStatus):
+        self.Name=Name
+        self.ExchangePriceScenario=ExchangePriceScenario
+        self.PrimaryReserveStatus=PrimaryReserveStatus
+
+class DistributionSystemOperator:
+    def __init__(self,Name,SwitchForGridFriendlyBehaviour,TimeWhenAllowedToCharge):
+        self.Name=Name
+        self.SwitchForGridFriendlyBehaviour=SwitchForGridFriendlyBehaviour
+        self.TimeWhenAllowedToCharge=TimeWhenAllowedToCharge
+
+
 #========================HouseHold Objects====================================================#
-
-H1=VPP.HouseHolds('House 1')
-pd=H1.PD('PD',0,30) # name,power,price
-dsm=H1.DSM('DSM',0,28) # name,power,price
-battery=H1.BatteryStorage('BatteryStorage',250,21,250,1,250,0,0,18,26,0) #name,power,price,rated_capacity_in_kWh,ageing_factor,usable_capacity_in_kWh,percentage_current_capacity,DOD,flag,charging_priority,charging_price,discharging_price,profit 
-pv=H1.SolarGeneratingUnit('SolarGeneratingUnit',400,1,0) # name, power,price 
-kwk=H1.KWK('KWK',0,14,0) # name, power,price
-
-#========================Grid Objects=================================================#
-
-common_grid=VPP.Grid.Common_Grid('Common_Grid',-10,0,12,30,0) #name, power,price, feed_in_price,draw_out_price,profit
+VPP=VirtualPowerPlant('VPP')
+H1=VPP.Household('House 1')
+StandardConsumingDevices=H1.StandardConsumingDevices('StandardConsumingDevices',10,30) # Name,Power,Price
+DSM=H1.DSM('DSM',0,28) # Name,Power,Price
+MarketBatteryStorage=H1.MarketBatteryStorage('MarketBatteryStorage',250,21,250,1,250,50,0,18,26,0) #Name,Power,Price,rated_capacity_in_kWh,AgeingFactor,UsableCapacityInKWh,PercentageCurrentCapacity,DOD,flag,charging_priority,ChargingPrice,disChargingPrice,Profit 
+pv=H1.MarketSolarGeneratingUnit('SolarGeneratingUnit',400,1,0) # Name, Power,Price 
+kwk=H1.MarketCogenerationUnit('MarketCogenerationUnit',0,14,0) # Name, Power,Price
+FeedInTariff=VirtualPowerPlant.H1.FeedInTariff('FeedInTariff',-10,0,12,30,0) #Name, Power,Price, FeedInPremier,draw_out_Price,Profit
